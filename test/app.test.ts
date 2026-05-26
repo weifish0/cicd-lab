8d987ced588f72be7de25cd9bbf236a7f041da2a
weifish0 <will060710@gmail.com>
2026-05-26 21:31:30 +0800

    fix: change test expect text

--- test/app.test.ts
+++ test/app.test.ts
@@ -22,7 +22,7 @@ describe('Fastify app', () => {
     });
 
     expect(response.statusCode).toBe(200);
-    expect(response.json().message).toBe('wrong message');
+    expect(response.json().message).toBe('CI/CD Lab Fastify app is running');
     await app.close();
   });
 });
